15d25eafb88302aa9e7acb5c009b02bcf74a4431
ONZ1 <onzi1230@gmail.com>
2021-04-06 00:49:59 +0900

    Create 003loop_2.cpp

---
+++ 003loop_2/003loop_2/003loop_2.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*
+		while문을 이용한 출력
+		2-1
+
+		*
+		***
+		*****
+		*******
+
+		*/
+	printf("\n[문제2-1]\n");
+
+	int starloop1 = 0;
+
+	while (starloop1 < 7)
+	{
+		int b = 0;
+		while (b <= starloop1)
+		{
+			printf("*");
+			b++;
+		}
+		printf("\n");
+		starloop1 += 2;
+	}
+
+	fgetc(stdin);
+	return 0;
+}
